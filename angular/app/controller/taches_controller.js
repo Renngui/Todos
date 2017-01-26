1daacf82027426333b4dcf686093e9c00cd257a4
Renngui <renngui@live.fr>
2017-01-26 12:26:56 +0100

    maj

--- angular/app/controller/taches_controller.js
+++ angular/app/controller/taches_controller.js
@@ -1,9 +1,10 @@
 angular.module('todos').controller('TacheController', ['$scope', '$http', 'Tache',
     function($scope, $http, Tache) {
         $scope.currentListe = Tache.currentListe;
-            $scope.$watch(function() {
-                return Tache.currentListe;
-            }, function(newValue, oldValue) {
+        $scope.$watch(function() {
+            return Tache.currentListe;
+        }, function(newValue, oldValue) {
+            if (newValue) {
                 $http.defaults.headers.common.Authorization = 'e136279df7a442bba03351d0350a5f3c';
                 $http.get('http://todos.api.netlor.fr/lists/' + newValue.id + '/todos')
                     .then(function(response) {
@@ -18,7 +19,8 @@ angular.module('todos').controller('TacheController', ['$scope', '$http', 'Tache
                         function(error) {
                             console.log(error);
                         });
-            });
+            }
+        });
 
 
 
